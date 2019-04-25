e4c2d8c862ee718078ff09e113969c27c2a5b47f
Gary Chen <gkchen@users.noreply.github.com>
2019-04-25 11:09:00 -0700

    changed autoreduce

--- nicepy/__init__.py
+++ nicepy/__init__.py
@@ -1,5 +1,5 @@
 import pint as _pint
-u = _pint.UnitRegistry(auto_reduce_dimensions=True)
+u = _pint.UnitRegistry(auto_reduce_dimensions=False)
 
 from nicepy.plotting import *
 from nicepy.maths import *
